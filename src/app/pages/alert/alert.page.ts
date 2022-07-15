c6c63ffea42b2e39fff913125bbd9c34ea76a90a
fernando.betancourt <bull-000@hotmail.com>
2022-07-15 17:26:34 -0500

    lists and avatar

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +9,130 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  handlerMessage = '';
+  roleMessage = '';
 
-  ngOnInit() {
+  constructor(private alertController: AlertController) { }
+
+  ngOnInit() { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
+  async presentAlert2() {
+    const alert = await this.alertController.create({
+      header: 'Alert!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => { this.handlerMessage = 'Alert2 canceled'; }
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => { this.handlerMessage = 'Alert2 confirmed'; }
+        }
+      ]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }
+
+  async presentAlert3() {
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      buttons: [
+        {
+          text: 'OK',
+          handler: (data) => {
+            console.log('ok clicked!', data);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancel clicked!');
+          }
+        },
+      ],
+      backdropDismiss: false,
+      inputs: [
+        {
+          placeholder: 'Name'
+        },
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8
+          }
+        },
+        {
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself'
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlert4() {
+    const alert = await this.alertController.create({
+      header: 'Select your favorite color',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('ok clicked!');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancel clicked!');
+          }
+        },
+      ],
+      inputs: [
+        {
+          label: 'Red',
+          type: 'radio',
+          value: 'red'
+        },
+        {
+          label: 'Blue',
+          type: 'radio',
+          value: 'blue'
+        },
+        {
+          label: 'Green',
+          type: 'radio',
+          value: 'green'
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
--- src/app/pages/index/index.page.ts
+++ src/app/pages/index/index.page.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface ListComponent {
+  icon: string;
+  name:  string;
+  redirectTo: string;
+  color: string;
+}
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
 })
 export class IndexPage implements OnInit {
+
+  listComponents: ListComponent[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet',
+      color: 'primary'
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirectTo: '/alert',
+      color: 'warning'
+    },
+    {
+      icon: 'beaker-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar',
+      color: 'tertiary'
+    },
+  ];
 
   constructor() { }
 
